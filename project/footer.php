b02581cd5c174bac48af09c6261dbb3803d2e1d6
Muhammad Atif <12020020050@umt.edu.pk>
2014-06-14 16:42:20 +0500

    first commit

---
+++ project/footer.php
@@ -0,0 +1,8 @@
+<div class="spacer"></div>
+			
+			<div id="footer">
+				Copyright &copy; 2014 Wordpress Theme by <a href="http://www.sst.umt.edu.pk">Saira Naz</a><br />
+			</div>
+			
+		</div><!-- content -->
+<?php get_sidebar(); ?>
---
+++ sidebar.php
@@ -0,0 +1,19 @@
+		<div id="sidebar">
+			<div class="sidebar-box">
+				<span class="sidebar-title">About Us</span>
+				<div class="dots"></div>			
+				<div class="textwidget">
+					<p>Project for the Subject Open source to create a worpdress theme</p>
+				</div>
+			</div>		
+			<div class="sidebar-box">		
+					<span class="sidebar-title">recent posts</span><div class="dots"></div>		
+				<ul>
+				<li><a href="http://sst.umt.edu.pk" title="Hello world!">Hello world!</a></li>
+				</ul>
+			</div>
+		</div><!-- sidebar -->		
+		<div class="spacer"></div>
+	</div><!-- wrapper -->
+	</body>
+</html>
